fe1e0982b04fe8edf9fb2ac7f56b40efd8cd054d
Guli Kholmatova <gulikholmatova@gmail.com>
2018-11-12 18:01:01 -0500

    Added data in seed.js

--- seed.js
+++ seed.js
@@ -1,12 +1,32 @@
-const { db, Gardener, Plot, Vegetable } = reqyire('./models.js');
+const { db, Gardener, Plot, Vegetable } = require('./models.js');
+
+const vegetableData = [
+  { name: 'Carrot', color: 'orange' },
+  { name: 'Tomato', color: 'red' },
+  { name: 'Pepper', color: 'green' },
+];
+const gardenerData = [
+  { name: 'Amanda', age: 27 },
+  { name: 'Guli', age: 25 },
+  { name: 'M', age: 5 },
+];
+const plotData = [
+  { size: 10, shaded: false },
+  { size: 5, shaded: true },
+  { size: 25, shaded: false },
+];
 
 db.sync({ force: true })
-  .then(data => {
-    // console.log('Database synced!');
-    // db.close();
+  .then(() => {
+    return Promise.all([
+      Vegetable.bulkCreate(vegetableData, { returning: true }),
+      Gardener.bulkCreate(gardenerData, { returning: true }),
+      Plot.bulkCreate(plotData, { returning: true }),
+    ]);
   })
-  .then(data => {
+  .then(() => {
     console.log('Database synced!');
+    db.close();
   })
   .catch(err => {
     console.log('DisaSter! Something went AWRY');
