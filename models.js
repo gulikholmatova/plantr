82a82161998fc6d0148215df8477d700f94802c6
Guli Kholmatova <gulikholmatova@gmail.com>
2018-11-12 17:13:45 -0500

    Awry!!!

--- models.js
+++ models.js
@@ -1,4 +1,20 @@
 const Sequelize = require('Sequelize');
 const db = new Sequelize('postgres://localhost:5432/plantr');
+const Gardener = db.define('gardener', {
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER,
+});
+const Plot = db.define('plot', {
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN,
+});
+const Vegetable = db.define('vegetable', {
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  plantedOn: Sequelize.DATE,
+});
+Vegetable.belongsToMany(Plot, { through: 'vegetable_plot' });
+Plot.belongsToMany(Vegetable, { through: 'vegetable_plot' });
+Gardener.belongsTo(Vegetable, { as: 'favorite_vegetable' });
 
 module.exports = db;
